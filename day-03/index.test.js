7c2045d007dc4e85782d8a46b1a91a24dd8edd6d
Sean Moriarty <spmoriarty1@gmail.com>
2022-09-22 15:52:50 -0700

    findPerson in tree tested and working

--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,6 +1,9 @@
 const {
   BinaryTreeNode
 } = require('./BinaryTree');
+const {
+  PersonTreeNode
+} = require('./FindPerson');
 
 
 describe('node based data structures', () => {
@@ -13,7 +16,19 @@ describe('node based data structures', () => {
     tree.add(1);
     tree.add(8);
     tree.add(9);
-    expect(tree).toEqual({ 'left': { 'left': null, 'right': null, 'value': 1 }, 'right': { 'left': null, 'right': { 'left': null, 'right': null, 'value': 9 }, 'value': 8 }, 'value': 5 });
+    expect(tree).toEqual({ 'left': { 'left': null, 'right': null, 'value': 1 },
+      'right': { 'left': null, 'right': { 'left': null, 'right': null, 'value': 9 },
+        'value': 8 }, 'value': 5 });
   });
 
+  it('This test will add and find a person in a tree route', () => {
+    const personA = { name: 'Peter' };
+    const personB = { name: 'Tom' };
+    const personTree = new PersonTreeNode(personA);
+    personTree.add(personB);
+    expect(personTree.findPerson('Queen Elizabeth II')).toBe('Not in tree');
+  });
+
+
 });
+
