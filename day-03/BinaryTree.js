c58b3fde02a5850775480c363c38419dbe554e50
Sean Moriarty <spmoriarty1@gmail.com>
2022-09-22 15:10:12 -0700

    working Binary tree and test

---
+++ day-03/BinaryTree.js
@@ -0,0 +1,39 @@
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+        
+}
+
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+      
+  add(val) {
+    if (val < this.value) {
+      if (!this.left) {
+        this.left = new BinaryTreeNode(val);
+      } else {
+        this.left.add(val);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BinaryTreeNode(val);
+      } else {
+        this.right.add(val);
+      }
+            
+    }     
+    
+  }
+}
+
+
+
+module.exports = { BinaryTreeNode, Node };
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,5 +1,19 @@
+const {
+  BinaryTreeNode
+} = require('./BinaryTree');
+
+
 describe('node based data structures', () => {
   it('example test', () => {
     expect(1).toBe(1);
   });
+
+  it('Should show B as the root of A,C,D', () => {
+    const tree = new BinaryTreeNode(5);
+    tree.add(1);
+    tree.add(8);
+    tree.add(9);
+    expect(tree).toEqual({ 'left': { 'left': null, 'right': null, 'value': 1 }, 'right': { 'left': null, 'right': { 'left': null, 'right': null, 'value': 9 }, 'value': 8 }, 'value': 5 });
+  });
+
 });
